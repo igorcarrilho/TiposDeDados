261a495227b9b656b93185625d80f74334950f1a
Igor <igor_g_carrilho@hotmail.com>
2022-08-27 18:33:42 -0300

    Tipos de dados em Java

---
+++ src/TipoDado.java
@@ -0,0 +1,22 @@
+public class TipoDado {
+    public static void  main(String[] args){
+        //Demonstração de alguns tipos de dados
+        int numero;
+        float decimalCurto;
+        double decimalLongo;
+        char caracter;
+        String cadeiaCaractere;
+
+        numero = 10;
+        decimalCurto = 10.5f;
+        decimalLongo = 20.5;
+        caracter = 'a';
+        cadeiaCaractere = "Igor Carrilho";
+
+        System.out.println(numero);
+        System.out.println(decimalCurto);
+        System.out.println(decimalLongo);
+        System.out.println(cadeiaCaractere);
+        System.out.println(caracter);
+    }
+}
